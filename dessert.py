e20bf4654212e2da126447b9d788a8d9dda47d2a
AldenRigby <alden.rigby@ucas-edu.net>
2024-10-22 11:47:12 -0600

    awefawef

--- dessert.py
+++ dessert.py
@@ -1,79 +1,33 @@
-class Store:
-    name = "BBQ Grill"
-    def __init__(self, id):
-        self.id = id
-        self.desserts = []
-        self.featuredDessert = None
-
-    def addDessert(self, dessert):
-        assert isinstance(dessert, DessertItem)
-        self.desserts.append(dessert)
-
-    def removeDessert(self, dessert):
-        try:
-            self.desserts.remove(dessert)
-        except:
-            print("nuh uh")
-        else:
-            print(dessert, "removed.")
-
-    def feature(self, name):
-        for dessert in self.desserts:
-            if dessert.name == name:
-                self.featuredDessert = name
-                print("Featured dessert is", name)
-                break
-        else:
-            print("nuh uh")
-    
-    def getFeatured(self):
-        return self.featuredDessert
-    
-    def store(self):
-        for dessert in self.desserts:
-            dessert.eat()
-
-    def getCandies(self):
-        return self.getByType(Candy)
-    def getCookies(self):
-        return self.getByType(Cookie)
-    def getByType(self, typ):
-        return [str(dessert) for dessert in self.desserts if isinstance(dessert, typ)]
 class DessertItem:
     def __init__(self, name):
         self.name = name
 
     def __str__(self):
         return f"Dessert: {self.name}"
-    
-    def eat(self):
-        print(self.name, "eating", self.diet)
 
 class Candy(DessertItem):
-    pass
-
-class Skittle(Candy):
-    diet = "mice"
-
-class KitKat(Candy):
-    diet = "potato"
+    def __init__(self, name, candyWeight, pricePerPound):
+        super().__init__(name)
+        self.candyWeight = candyWeight
+        self.pricePerPound = pricePerPound
 
 class Cookie(DessertItem):
-    pass
+    def __init__(self, name, cookieQuantity, pricePerDozen):
+        super().__init__(name)
+        self.cookieQuantity = cookieQuantity
+        self.pricePerPound = pricePerDozen
 
-class Snickerdoodle(Cookie):
-    diet = "rodents"
+class IceCream(DessertItem):
+    def __init__(self, name, scoopCount, pricePerScoop):
+        super().__init__(name)
+        self.scoopCount = scoopCount
+        self.pricePerScoop = pricePerScoop
 
-class ChocolateChip(Cookie):
-    diet = "carrots"
+class Sundae(IceCream):
+    def __init__(self, name, scoopCount, pricePerScoop, toppingName, toppingPrice):
+        super().__init__(name, scoopCount, pricePerScoop)
+        self.toppingName = toppingName
+        self.toppingPrice = toppingPrice
 
-store = Store(1)
-store.addDessert(Snickerdoodle("awefawefawefawefwefawef"))
-store.addDessert(Skittle("whee hee hee hee"))
-store.addDessert(KitKat("potato"))
-store.addDessert(ChocolateChip("google en passant"))
-
-store.feature("potato")
-store.store()
-
-print(store.getByType(Cookie))
+awef = Sundae("skittle", 100, 1, "awef", 3243)
+print(awef)
